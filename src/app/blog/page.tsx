8b5ca7d044998e30789d9555ba7d1694d0c4e09c
ashish11011 <bishnoi11011@gmail.com>
2025-05-17 18:10:39 +0530

    add blog metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,8 +6,22 @@ import Link from 'next/link';
 import { getAllBlogsMetaDeta } from './../../../lib/index';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import { Metadata } from 'next';
 
 dayjs.extend(utc);
+
+export const metadata: Metadata = {
+  title: 'Blogs',
+  description:
+    'Read all the lastest trends occurring in the it sector from AI to no-code development we cover all. Vist to know more',
+  alternates: {
+    canonical: 'https://www.avtechnosys.com/blog',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 const Blog = async () => {
   const blogData = await getAllBlogsMetaDeta();
