ce7b975ce87757f24ccad2edf7df67a4c66ebeb3
Av-technosys <Avtechnosys02@gmail.com>
2025-08-22 11:18:27 +0530

    add clarity script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "synyul31ml");
+        </script>
         <link rel="icon" type="image/x-icon" href="/new/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
