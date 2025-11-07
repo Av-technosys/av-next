b66157de8ea8715b338be2beb4d27a788fb48f31
ashish11011 <bishnoi11011@gmail.com>
2025-11-07 16:45:12 +0530

    remove top companies

--- src/app/(services)/mobile-app-development/TopCompanies.tsx
+++ src/app/(services)/mobile-app-development/TopCompanies.tsx
@@ -4,33 +4,34 @@ import React from 'react';
 
 const TopCompanies = ({ className }: { className?: string }) => {
   return (
-    <div className={className}>
-      <div
-        className={cn('mx-auto flex w-full max-w-7xl items-center px-4 py-6')}
-      >
-        <div className="h-0.5 w-full flex-1 rounded bg-gray-300"></div>
-        <div className="px-6 text-center font-semibold text-neutral-600">
-          Trusted by Corporations, Enterprises, And Startups.
-        </div>
-        <div className="h-0.5 w-full flex-1 rounded bg-gray-300"></div>
-      </div>
-      <div className="mx-auto grid w-full max-w-7xl grid-cols-2 gap-6 px-4 py-3 sm:grid-cols-3 md:grid-cols-5">
-        {logoData.map((item, index) => (
-          <div
-            key={index}
-            className="w-full flex-1 rounded-md border px-4 py-2 shadow"
-          >
-            <Image
-              width={100}
-              height={100}
-              src={item}
-              className="m-auto h-10 w-auto"
-              alt=""
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    <></>
+    // <div className={className}>
+    //   <div
+    //     className={cn('mx-auto flex w-full max-w-7xl items-center px-4 py-6')}
+    //   >
+    //     <div className="h-0.5 w-full flex-1 rounded bg-gray-300"></div>
+    //     <div className="px-6 text-center font-semibold text-neutral-600">
+    //       Trusted by Corporations, Enterprises, And Startups.
+    //     </div>
+    //     <div className="h-0.5 w-full flex-1 rounded bg-gray-300"></div>
+    //   </div>
+    //   <div className="mx-auto grid w-full max-w-7xl grid-cols-2 gap-6 px-4 py-3 sm:grid-cols-3 md:grid-cols-5">
+    //     {logoData.map((item, index) => (
+    //       <div
+    //         key={index}
+    //         className="w-full flex-1 rounded-md border px-4 py-2 shadow"
+    //       >
+    //         <Image
+    //           width={100}
+    //           height={100}
+    //           src={item}
+    //           className="m-auto h-10 w-auto"
+    //           alt=""
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
